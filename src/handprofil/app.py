d5b55c3b83705da8d9770bf44ca84a1ca4e54c88
Daniel Steinmann <dsteinmann@techgroup.ch>
2024-03-18 16:50:20 +0100

    moved layouts to separate file

--- src/handprofil/app.py
+++ src/handprofil/app.py
@@ -10,7 +10,7 @@ from flask import send_file
 
 from calculations import get_calculated_values
 from excelparser import parse_contents, validate_upload, split_metadata_data
-from plotting import return_figure
+from plotting import return_figure, get_layout
 from common import (
     get_absolute_path,
     load_meta_attributes,
@@ -25,6 +25,7 @@ def load_plot_section_config():
     ) as file:
         sections = json.load(file)
     return sections
+
 
 # Path
 print(os.getenv("DEBUG", "NONE"))
@@ -59,112 +60,7 @@ initial_df = pd.DataFrame(
 )
 
 # App layout
-app.layout = dmc.Container(
-    [
-        dmc.Title("Handlabor"),
-        html.Div(
-            [
-                dmc.Button("Download Template (.xlsx)", id="btn_image"),
-                dcc.Download(id="download-xlsx"),
-            ]
-        ),
-        dmc.Title("Upload", order=2),
-        dcc.Upload(
-            id="upload-data",
-            children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
-            style={
-                "width": "100%",
-                "height": "60px",
-                "lineHeight": "60px",
-                "borderWidth": "1px",
-                "borderStyle": "dashed",
-                "borderRadius": "5px",
-                "textAlign": "center",
-                "margin": "10px",
-            },
-            # Allow multiple files to be uploaded
-            multiple=True,
-        ),
-        dmc.Container(id="upload-alerts"),
-        dmc.Title("Datensätze", order=2),
-        dmc.Grid(id="subject-grid"),
-        dmc.Grid(
-            [
-                dmc.Col(
-                    [
-                        dmc.Card(
-                            children=[
-                                dmc.Title("Hintergrund", order=2),
-                                dmc.RadioGroup(
-                                    [
-                                        dmc.Radio(l, value=k)
-                                        for k, l in [
-                                            ["m", "Männlich"],
-                                            ["w", "Weiblich"],
-                                        ]
-                                    ],
-                                    id="radiogroup-sex",
-                                    value="m",
-                                    label="Geschlecht",
-                                    size="sm",
-                                    mt=10,
-                                ),
-                                dmc.Select(
-                                    label="Instrument",
-                                    searchable=True,
-                                    placeholder="Select one",
-                                    id="select-instrument",
-                                    value="gemischt",
-                                    data=[
-                                        {"value": "violine", "label": "Violine"},
-                                        {
-                                            "value": "violoncello",
-                                            "label": "Violoncello",
-                                        },
-                                        {"value": "schlagzeug", "label": "Schlagzeug"},
-                                        {"value": "klavier", "label": "Klavier"},
-                                        {"value": "gitarre", "label": "Gitarre"},
-                                        {"value": "egitarre", "label": "E-Gitarre"},
-                                        {"value": "akkordeon", "label": "Akkordeon"},
-                                        {"value": "gemischt", "label": "Gemischt"},
-                                    ],
-                                    style={"width": 200, "marginBottom": 10},
-                                ),
-                            ]
-                        ),
-                    ],
-                    span="auto",
-                ),
-                dmc.Col(
-                    [
-                        dmc.Card(
-                            children=[
-                                dmc.Title("Hand", order=2),
-                                dmc.RadioGroup(
-                                    [
-                                        dmc.Radio(l, value=k)
-                                        for k, l in [
-                                            ["right", "Rechts"],
-                                            ["left", "Links"],
-                                        ]
-                                    ],
-                                    id="radiogroup-hand",
-                                    value="right",
-                                    label="Hand",
-                                    size="sm",
-                                    mt=10,
-                                ),
-                            ]
-                        ),
-                    ],
-                    span="auto",
-                ),
-            ]
-        ),
-        html.Div(id="all_plots"),
-    ],
-    fluid=True,
-)
+app.layout = get_layout()
 
 
 @callback(
--- src/plotting/__init__.py
+++ src/plotting/__init__.py
@@ -1,1 +1,2 @@
 from .figures import return_figure
+from .layout import get_layout
