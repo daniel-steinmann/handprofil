5fbdf18260f8440e5aa52efc2b8aee67bfe636a1
Daniel Steinmann <dsteinmann@techgroup.ch>
2024-03-19 13:47:21 +0100

    korrekte darstellung

--- src/plotting/sections.py
+++ src/plotting/sections.py
@@ -5,6 +5,29 @@ import common
 import plotting
 
 
+def load_plot_section_config() -> list:
+    with open(
+        common.get_absolute_path("src/plotting/config/plot_sections.json"), "r"
+    ) as file:
+        section_config = json.load(file)
+    return section_config
+
+
+def get_plot_sections(values, attributes, section_config):
+    merged = attributes.merge(values.rename(
+        'value'), left_index=True, right_index=True)
+    data_per_section = split_reorder_plot_df_to_sections(
+        merged, section_config)
+
+    all_plots_children = []
+    for index, section in enumerate(section_config):
+        figure = plotting.return_section_figure(data_per_section[index])
+        child = plotting.wrap_figure_in_graph(section["title"], figure)
+        all_plots_children.append(child)
+
+    return all_plots_children
+
+
 def split_reorder_plot_df_to_sections(plot_df: pd.DataFrame, section_config: list):
     plot_df["id"] = plot_df.index.values
     output_dfs = []
@@ -16,26 +39,3 @@ def split_reorder_plot_df_to_sections(plot_df: pd.DataFrame, section_config: lis
         output_dfs.append(output_df)
 
     return output_dfs
-
-
-def load_plot_section_config() -> list:
-    with open(
-        common.get_absolute_path("src/plotting/config/plot_sections.json"), "r"
-    ) as file:
-        section_config = json.load(file)
-    return section_config
-
-
-def get_plot_sections(values, attributes, section_config):
-
-    attributes["value"] = values
-    data_per_section = split_reorder_plot_df_to_sections(
-        attributes, section_config)
-
-    all_plots_children = []
-    for index, section in enumerate(section_config):
-        figure = plotting.return_section_figure(data_per_section[index])
-        child = plotting.wrap_figure_in_graph(section["title"], figure)
-        all_plots_children.append(child)
-
-    return all_plots_children
